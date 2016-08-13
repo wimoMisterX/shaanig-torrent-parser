656198dfa60ce79055df7a940c711145fe9eff9d
wimoMisterX <ran12us@gmail.com>
2016-08-13 12:36:46 +0800

    Added while loop through pages and clears previous output

--- scrapper.py
+++ scrapper.py
@@ -1,12 +1,30 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-page = urllib2.urlopen('https://www.shaanig.org/f30/index1.html')
-soup = BeautifulSoup(page, 'lxml')
+user_response = ''
+page_no = 1
+movie_list = {}
+CURSOR_UP_ONE = '\x1b[1A'
+ERASE_LINE = '\x1b[2K'
 
-threads = soup.body.find('ol', {'id': 'threads'})
-movie_list = {thread.find('a', {'class': 'title'}).string: thread.find('a', {'class': 'title'})['href'] for thread in threads.find_all('li', {'class': 'threadbit'})}
-for movie in movie_list.keys():
-    print '[' + str(movie_list.keys().index(movie)+1) + '] ' + movie
-movie_choice = raw_input("Please enter your choice: ")
-print movie_list[movie_list.keys()[int(movie_choice)-1]]
+while user_response != 'n':
+    page = urllib2.urlopen('https://www.shaanig.org/f30/index' + str(page_no)  + '.html')
+    soup = BeautifulSoup(page, 'lxml')
+    threads = soup.body.find('ol', {'id': 'threads'})
+    current_list = {
+        thread.find('a', {'class': 'title'}).string: thread.find('a', {'class': 'title'})['href']
+        for thread in threads.find_all('li', {'class': 'threadbit'})
+    }
+    for movie in current_list.keys():
+        print '[' + str(current_list.keys().index(movie)+1) + '] ' + movie
+    movie_list.update(current_list)
+    movie_choice = raw_input("Please enter your choice (-1 to stop): ")
+    while int(movie_choice) > 0 and int(movie_choice) < 31:
+        print movie_list[movie_list.keys()[int(movie_choice)-1]]
+        for x in range(1):
+            print CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE
+        movie_choice = raw_input("Please enter your choice: ")
+    user_response = raw_input("Go to next page(y/n): ")
+    page_no += 1
+    for x in range(len(current_list) + 3):
+        print CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE
